1d0ced314aca78487e48b60edff7075710c3af45
iiekezie <iiekezie@gmail.com>
2023-11-29 02:56:33 -0800

    0x01-python-if_else_loops_functions

---
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# 11-pow.py
+# Ifeanyi I Ekezie
+def pow(a, b):
+    result = a ** b
+    return result
+
+# Uncomment the following lines for testing
+# print(pow(2, 2))
+# print(pow(98, 2))
+# print(pow(98, 0))
+# print(pow(100, -2))
+# print(pow(-4, 5))
