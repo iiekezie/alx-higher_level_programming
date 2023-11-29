fa29f4a89a3062189adfe157432ff4dbc81a6c84
iiekezie <iiekezie@gmail.com>
2023-11-29 02:50:47 -0800

    0x01-python-if_else_loops_functions

---
+++ 0x01-python-if_else_loops_functions/10-add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# 10-add.py
+# Ifeanyi I Ekezie
+def add(a, b):
+    result = a + b
+    return result
+
+# Uncomment the following lines for testing
+# print(add(1, 2))
+# print(add(98, 0))
+# print(add(100, -2))
