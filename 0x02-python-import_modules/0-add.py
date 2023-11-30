88bf11cad65adc9787cf73b74729422013cf165b
iiekezie <iiekezie@gmail.com>
2023-11-30 04:39:19 -0800

    0x02-python-import_modules

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 # 0-add.py
 # Ifeanyi I Ekezie
-from add_0 import add
-
-a = 1
-b = 2
-c = add(a, b)
-
-print("{} + {} = {}".format(a, b, c))
+if __name__ == "__main__":
+    from add_0 import add
+    a = 1
+    b = 2
+    result = add(a, b)
+    print("{0} + {1} = {2}".format(a, b, result))
