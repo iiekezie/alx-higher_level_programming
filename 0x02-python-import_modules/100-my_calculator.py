4078bafae224c5335c9dce7a7327f678d7ec1689
iiekezie <iiekezie@gmail.com>
2023-12-01 00:22:54 -0800

    0x02-python-import_modules

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,14 +1,31 @@
 #!/usr/bin/python3
+import sys
+from calculator_1 import add, sub, mul, div
 
-def magic_calculation(a, b):
-    """Handle basic arithmetic operations."""
-    from magic_calculation_102 import add, sub
+def calculator():
+    if len(sys.argv) != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        sys.exit(1)
 
-    if a < b:
-        c = add(a, b)
-        for letter in range(4, 6):
-            c = add(c, letter)
-        return (c)
+    a = int(sys.argv[1])
+    operator = sys.argv[2]
+    b = int(sys.argv[3])
 
-    else:
-        return(sub(a, b))
+    if operator not in ['+', '-', '*', '/']:
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
+
+    result = 0
+    if operator == '+':
+        result = add(a, b)
+    elif operator == '-':
+        result = sub(a, b)
+    elif operator == '*':
+        result = mul(a, b)
+    elif operator == '/':
+        result = div(a, b)
+
+    print(f"{a} {operator} {b} = {result}")
+
+if __name__ == "__main__":
+    calculator()
