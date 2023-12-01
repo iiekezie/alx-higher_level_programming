43424b382178e46922e000454c43f4ccaf27503e
iiekezie <iiekezie@gmail.com>
2023-12-01 00:27:02 -0800

    0x02-python-import_modules

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -7,9 +7,7 @@ def calculator():
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
 
-    a = int(sys.argv[1])
-    operator = sys.argv[2]
-    b = int(sys.argv[3])
+    a, operator, b = map(int, sys.argv[1:4])
 
     if operator not in ['+', '-', '*', '/']:
         print("Unknown operator. Available operators: +, -, * and /")
