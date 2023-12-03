4cce4c09b04e13c64af269b9151ff43610fd98cd
iiekezie <iiekezie@gmail.com>
2023-12-03 00:06:13 -0800

    0x02-python-import_modules

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,20 +1,32 @@
 #!/usr/bin/python3
+# 100-my_calculator.py
+# Ifeanyi I Ekezie
+import sys
+from calculator_1 import add, sub, mul, div
 
 if __name__ == "__main__":
-    """Handle basic arithmetic operations."""
-    from calculator_1 import add, sub, mul, div
-    import sys
-
-    if len(sys.argv) - 1 != 3:
+    if len(sys.argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
 
-    ops = {"+": add, "-": sub, "*": mul, "/": div}
-    if sys.argv[2] not in list(ops.keys()):
+    a = int(sys.argv[1])
+    op = sys.argv[2]
+    b = int(sys.argv[3])
+
+    if op not in ('+', '-', '*', '/'):
         print("Unknown operator. Available operators: +, -, * and /")
         sys.exit(1)
 
+    result = None
 
-    a = int(sys.argv[1])
-    b = int(sys.argv[3])
-    print("{} {} {} = {}".format(a, sys.argv[2], b, ops[sys.argv[2]](a, b)))
+    if op == '+':
+        result = add(a, b)
+    elif op == '-':
+        result = sub(a, b)
+    elif op == '*':
+        result = mul(a, b)
+    elif op == '/':
+        result = div(a, b)
+
+    print(f"{a} {op} {b} = {result}")
+    sys.exit(0)
