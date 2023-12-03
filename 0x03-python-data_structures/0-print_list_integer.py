1221378a667e4f248e9c1d401ed7e7848c0da7fb
iiekezie <iiekezie@gmail.com>
2023-12-03 02:13:24 -0800

    Print a list of integers

---
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+def print_list_integer(my_list=[]):
+    """
+    Print integers of a list.
+
+    :param my_list: List of integers
+    :type my_list: list
+    """
+    for i in my_list:
+        print("{:d}".format(i))
+
+# Example usage
+if __name__ == "__main__":
+    my_list = [1, 2, 3, 4, 5]
+    print_list_integer(my_list)
