06ceb3c8db77e1a781214f58761b7d549a6331d9
iiekezie <iiekezie@gmail.com>
2023-12-05 01:11:49 -0800

    Replace in a copy

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    a = tuple_a + (0, 0)
-    b = tuple_b + (0, 0)
-    return (a[0] + b[0], a[1] + b[1])
-
-tuple_a = (1, 89)
-tuple_b = (88, 11)
-new_tuple = add_tuple(tuple_a, tuple_b)
-print(new_tuple)
-
-print(add_tuple(tuple_a, (1, )))
-print(add_tuple(tuple_a, ()))
+    # Initialize the result tuple with two zeros
+    result = (0, 0)
+    for i in range(2):
+        # Get the element from tuple_a or 0 if it does not exist
+        a = tuple_a[i] if i < len(tuple_a) else 0
+        b = tuple_b[i] if i < len(tuple_b) else 0
+        result = result[:i] + (a + b,) + result[i+1:]
+    # Return the result tuple
+    return result
