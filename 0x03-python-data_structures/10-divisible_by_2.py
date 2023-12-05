945ec91fad24ab054dbb6a71d322b86fa45ab898
iiekezie <iiekezie@gmail.com>
2023-12-05 02:26:07 -0800

    Find the max

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -5,11 +5,8 @@ def divisible_by_2(my_list=[]):
     for i in copy_list:
         # Check if the element is divisible by 2
         if i % 2 is 0:
-            # Append True to the result list
             list_result[counter] = True
         else:
-            # Append False to the result list
             list_result[counter] = False
         counter = counter + 1
-    # Return the result list
     return list_result
