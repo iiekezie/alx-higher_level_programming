fdcd2acd6127540632916300b7528eb827eb51fb
iiekezie <iiekezie@gmail.com>
2023-12-05 07:47:24 -0800

    Correction

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    """Prints a matrix of integers.
-
-    Args:
-        matrix (list of lists of ints, optional): The matrix to print. Defaults to [[]].
-    """
     for row in matrix:
-        # Print each item of the row, separated by a space, and formatted as an integer
-        print(" ".join("{:d}".format(item) for item in row))
+        for i, elem in enumerate(row):
+            if i != len(row) - 1:
+                print("{:d} ".format(elem), end="")
+            else:
+                print("{:d}".format(elem), end="")
+        print()
