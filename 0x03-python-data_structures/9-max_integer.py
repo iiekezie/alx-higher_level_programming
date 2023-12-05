b8d33e665702bbe5b0c29e3eabc45a1d04036207
iiekezie <iiekezie@gmail.com>
2023-12-05 02:10:01 -0800

    Find the max

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,10 +1,16 @@
 #!/usr/bin/python3
-def max_value_integer(my_list=[]):
+# 9-max_integer.py
+# Ifeanyi I Ekezie
+
+
+def max_integer(my_list=[]):
+    """Initialize the max value to None"""
     if len(my_list) == 0:
-        return "None"
-    else:
-        max_value = my_list[0]
-        for i in range(len(my_list)):
-            if my_list[i] > max_value:
-                max_value = my_list[i]
-        return max_value
+        return (None)
+
+    max_value = my_list[0]
+    for i in range(len(my_list)):
+        if my_list[i] > max_value:
+            max_value = my_list[i]
+
+    return (max_value)
