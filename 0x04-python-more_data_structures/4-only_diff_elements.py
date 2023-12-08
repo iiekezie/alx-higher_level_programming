8165c26bedcef5b6894c97de952050da10c223a6
iiekezie <iiekezie@gmail.com>
2023-12-08 03:36:45 -0800

    Only differents

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
 def only_diff_elements(set_1, set_2):
-    # Use the symmetric difference operator ^ to get the elements that are in either set but not both
     return set_1 ^ set_2
