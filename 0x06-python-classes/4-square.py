4be5c8717f401f5d58bbc70a318252a5d91d8d50
iiekezie <iiekezie@gmail.com>
2024-01-08 22:40:15 -0800

    Classes and Objects

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,20 +1,38 @@
 #!/usr/bin/python3
-"""Define a class Square."""
+"""
+Square Module - for all your needs, as long as they include printing squares
+"""
 
 
 class Square:
+    """
+    Square class now has a setters and getters for the size
+    amazing and new
+    """
+    def __init__(self, size=0):
+        """
+        Square class init! takes in a size
+        """
+        # methos that inherit from the class
         self.size = size
 
     @property
     def size(self):
-        """Get the current size of the square."""
+        """Get/set the current size of the square."""
         return (self.__size)
 
     @size.setter
     def size(self, value):
-        if not isinstance(value, int):
+        """
+        Size setter, sets the size
+        """
+        # it has to be an integer
+        if type(value) != int:
+            # otherwhise raise a TypeError
             raise TypeError("size must be an integer")
-        elif value < 0:
+        # if size is less than 0
+        if value < 0:
+            # raise a ValueError
             raise ValueError("size must be >= 0")
         self.__size = value
 
