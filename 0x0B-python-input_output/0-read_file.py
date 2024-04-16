eea180c327ef1ff7a3b43be2bab344eb056d669e
iiekezie <iiekezie@gmail.com>
2024-04-16 05:00:52 -0700

    Python - Inheritance

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -3,9 +3,6 @@
 
 
 def read_file(filename=""):
-    """
-    read_file: read a file using
-    with statement
-    """
-    with open(filename, 'read', encoding="utf-8") as file:
-        print(file.read(), end="")
+    """Print the contents of a UTF8 text file to stdout."""
+    with open(filename, encoding="utf-8") as f:
+        print(f.read(), end="")
