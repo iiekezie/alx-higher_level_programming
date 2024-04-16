b89ec2a3e60e3a8894a7dbeffabe41c3f9ff8d44
iiekezie <iiekezie@gmail.com>
2024-04-16 04:51:47 -0700

    Python - Inheritance

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-"""Defines a text file-reading function."""
+"""Module defining a function to read a text file and print its contents to stdout"""
 
 
 def read_file(filename=""):
-    with open(filename, encoding="utf-8") as f:
-        print(f.read(), end="")
-
-# Example usage
-if __name__ == "__main__":
-    read_file("my_file_0.txt")
+    """
+    read_file: read a file using
+    with statement
+    """
+    with open(filename, 'r', encoding="utf-8") as file:
+        print(file.read(), end="")
