13c487f4ed54c8885d5d55f24edad9ef35d542ef
iiekezie <iiekezie@gmail.com>
2024-04-16 05:10:55 -0700

    Python - Input/Output

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,17 +1,11 @@
 #!/usr/bin/python3
-
 """Module defining a function to write a string to a text file"""
 
+def write_file(filename="", text=""):
+    """Writes a string to a text file (UTF8) and returns the number of characters written"""
+    with open(filename, mode='w', encoding='utf-8') as file:
+        return file.write(text)
 
-
-def read_lines(filename="", nb_lines=0):
-    """
-    return lines readed
-    """
-    with open(filename, mode='r', encoding="utf-8") as f:
-        #  nb_lines is lower or equal to 0
-        if nb_lines <= 0:
-            print(f.read(), end="")
-        else:
-            for i in range(nb_lines):
-                print(f.readline(), end="")
+if __name__ == "__main__":
+    nb_characters = write_file("my_first_file.txt", "This School is so cool!\n")
+    print(nb_characters)
