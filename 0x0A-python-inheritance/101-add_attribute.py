26d620020663c2c1b2030d12cb755f738d5de8bf
iiekezie <iiekezie@gmail.com>
2024-04-16 04:45:01 -0700

    Python - Inheritance

---
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Module defining a function to add attribute to an object"""
+
+def add_attribute(obj, attribute, value):
+    """Adds a new attribute to an object if possible"""
+    if not hasattr(obj, "__dict__"):
+        raise TypeError("can't add new attribute")
+    setattr(obj, attribute, value)
+
+if __name__ == "__main__":
+    class MyClass():
+        pass
+
+    mc = MyClass()
+    add_attribute(mc, "name", "John")
+    print(mc.name)
+
+    try:
+        a = "My String"
+        add_attribute(a, "name", "Bob")
+        print(a.name)
+    except Exception as e:
+        print("[{}] {}".format(e.__class__.__name__, e))
