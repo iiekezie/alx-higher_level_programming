142638bc9660f5abe56818b81a60f793ce6e673b
iiekezie <iiekezie@gmail.com>
2024-05-23 10:26:09 -0700

    SQL - More queries

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,6 +1,5 @@
--- lists all shows contained in hbtn_0d_tvshows that have at least one genre linked
--- lists all rows of a database that have one column in common
-SELECT tv_shows.title, tv_show_genres.genre_id
+-- Script that lists all cities contained in the database
+SELECT tv_shows.title, tv_show_genres.genre_id -- Query to join cities and states
 FROM tv_shows
-JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
-ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
+JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+ORDER BY tv_shows.title, tv_show_genres.genre_id;
