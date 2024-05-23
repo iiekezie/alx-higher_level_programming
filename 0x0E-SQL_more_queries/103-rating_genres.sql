6e8c195fc4b6dc220f3c0243be0a60a09db991ea
iiekezie <iiekezie@gmail.com>
2024-05-23 10:44:36 -0700

    SQL - More queries

--- 0x0E-SQL_more_queries/103-rating_genres.sql
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -1,8 +1,9 @@
+
 -- lists all genres in the database hbtn_0d_tvshows_rate by their rating
 -- lists all rows in a database linked to a row in another table
-SELECT tv_genres.name, SUM(tv_shows.rating) AS rating_sum
+SELECT name, SUM(tv_show_ratings.rate) 'rating'
 FROM tv_genres
-JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
-JOIN tv_shows ON tv_show_genres.tv_show_id = tv_shows.id
-GROUP BY tv_genres.name
-ORDER BY rating_sum DESC;
+INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+INNER JOIN tv_show_ratings ON tv_show_genres.show_id = tv_show_ratings.show_id
+GROUP BY name
+ORDER BY rating DESC;
--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -1,7 +1,9 @@
 -- Script that lists genres of Dexter show
-SELECT tv_shows.title
+SELECT tv_shows.title -- Query to join tables
 FROM tv_shows
-JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
-JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
-WHERE tv_genres.name = 'Comedy'
-ORDER BY tv_shows.title ASC;
+     JOIN tv_show_genres
+     	  ON tv_show_genres.show_id = tv_shows.id
+     JOIN tv_genres
+     	  ON tv_genres.id = tv_show_genres.genre_id
+WHERE tv_genres.name = "Comedy"
+ORDER BY tv_shows.title;
