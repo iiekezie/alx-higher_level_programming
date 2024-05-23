401ad58d0c85a0444a287fc1c8bc3908d9444bac
iiekezie <iiekezie@gmail.com>
2024-05-23 10:01:58 -0700

    README.md File

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,1 +1,4 @@
-SELECT * FROM mysql.user WHERE User IN ('user_0d_1', 'user_0d_2');
+-- Script that lists all privileges of the MySQL users
+-- Query to list all privileges (GRANT) of the MySQL users
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
