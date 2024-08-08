7be9f53b3d01b1a0b0d3d76eaeec786aa9fd0d45
iiekezie <iiekezie@gmail.com>
2024-08-08 20:09:25 +0100

    JavaScript - Web scraping

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,20 +1,23 @@
 #!/usr/bin/node
-const request = require('request');
-const url = 'https://swapi.co/api/films/' + process.argv[2];
-request(url, function (error, response, body) {
-  if (!error) {
-    let characters = JSON.parse(body).characters;
-    printCharacters(characters, 0);
-  }
-});
 
-function printCharacters (characters, index) {
-  request(characters[index], function (error, response, body) {
-    if (!error) {
-      console.log(JSON.parse(body).name);
-      if (index + 1 < characters.length) {
-        printCharacters(characters, index + 1);
-      }
-    }
+const axios = require('axios');
+
+const movieId = process.argv[2];
+const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+
+axios.get(url)
+  .then(response => {
+    const characters = response.data.characters;
+
+    const characterRequests = characters.map(characterUrl => {
+      return axios.get(characterUrl).then(characterResponse => characterResponse.data.name);
+    });
+
+    return Promise.all(characterRequests);
+  })
+  .then(characterNames => {
+    characterNames.forEach(name => console.log(name));
+  })
+  .catch(error => {
+    console.error(error);
   });
-}
